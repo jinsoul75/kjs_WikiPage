641b7579d9912f0b3567c3808249ce53e38fd799
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2024-01-28 21:41:09 +0900

    fix: TypeError 해결

--- src/components/list/PostList.tsx
+++ src/components/list/PostList.tsx
@@ -5,7 +5,7 @@ import PostItem from './PostItem';
 export default function PostList({ posts, ...props }: { posts: Posts }) {
   return (
     <ul key="post-list" className="grid grid-rows-5 my-4 gap-4" {...props}>
-      {posts.map((post: Post) => {
+      {posts?.map((post: Post) => {
         return <PostItem key={post.id} post={post} />;
       })}
     </ul>
