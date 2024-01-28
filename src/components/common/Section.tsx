a0601653666725b64209053b650b8deb85c00f1b
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2024-01-28 21:40:35 +0900

    chore: section className props 추가

--- src/components/common/Section.tsx
+++ src/components/common/Section.tsx
@@ -1,8 +1,16 @@
 import { ReactNode } from 'react';
 
-export default function Section({ children }: { children: ReactNode }) {
+export default function Section({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
   return (
-    <section className="flex flex-col w-3/4 max-w-3xl min-h-96 m-8 rounded-md border border-gray-light bg-white p-4">
+    <section
+      className={`flex flex-col w-3/4 max-w-3xl min-h-96 m-8 rounded-md border border-gray-light bg-white p-4 ${className}`}
+    >
       {children}
     </section>
   );
