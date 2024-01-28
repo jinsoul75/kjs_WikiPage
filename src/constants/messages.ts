50d3c106e3becd2d28c15f9fd38587ba7b6139e3
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2024-01-28 23:06:42 +0900

    chore: key prop 변수처리

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -5,3 +5,5 @@ export const MISSING_INPUT = '제목과 내용을 모두 입력해주세요.';
 export const POST_CONFIRM = '게시물을 정말 업로드하시겠습니까?';
 
 export const TITLE_PLACEHOLDER = '제목을 입력하세요.';
+
+export const NO_WIKI_CONTENT = '등록된 위키가 없습니다.';
