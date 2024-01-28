cacc569bcc64568d9e076c0f87d691f772b2946e
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2024-01-28 23:06:08 +0900

    chore: 컨텐츠 없을 때 처리

--- src/components/list/PostList.tsx
+++ src/components/list/PostList.tsx
@@ -2,12 +2,22 @@ import { Post, Posts } from '@/types/types';
 
 import PostItem from './PostItem';
 
+import { NO_WIKI_CONTENT } from '@/constants/messages';
+
 export default function PostList({ posts, ...props }: { posts: Posts }) {
   return (
-    <ul key="post-list" className="grid grid-rows-5 my-4 gap-4" {...props}>
-      {posts?.map((post: Post) => {
-        return <PostItem key={post.id} post={post} />;
-      })}
-    </ul>
+    <>
+      {posts ? (
+        <ul className="grid grid-rows-5 my-4 gap-4" {...props}>
+          {posts?.map((post: Post) => {
+            return <PostItem key={post.id} post={post} />;
+          })}
+        </ul>
+      ) : (
+        <div className="flex grow justify-center items-center">
+          <div>{NO_WIKI_CONTENT}</div>
+        </div>
+      )}
+    </>
   );
 }
