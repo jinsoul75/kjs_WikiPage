893f5548b6249fbaaa6db2428bc87348f2c89c21
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2024-01-28 23:28:56 +0900

    fix: 링크 클릭시 새탭 생성 방지

--- src/components/postEdit/Editor.tsx
+++ src/components/postEdit/Editor.tsx
@@ -6,6 +6,26 @@ import dynamic from 'next/dynamic';
 
 import { Post } from '@/types/types';
 import { EDITOR_PLACEHOLDER } from '@/constants/messages';
+import { Quill } from 'react-quill';
+
+const Link = Quill.import("formats/link");
+
+class MyLink extends Link {
+    static create(value: any) {
+        const node = Link.create(value);
+        value = Link.sanitize(value);
+        node.setAttribute('href', value);
+        node.removeAttribute('target');
+        return node;
+    }
+
+    format(name:any, value: any) {
+        super.format(name, value);
+        this["domNode"].removeAttribute("target");
+    }
+}
+
+Quill.register(MyLink);
 
 interface EditorProps {
   post?: Post;
